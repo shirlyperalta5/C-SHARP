40b3ec3bff09cfc33698af854d0c588e8a453b1c
shirlyperalta5 <shirlyperalta5@gmail.com>
2022-06-17 18:25:16 -0600

    agregando proyecto1

---
+++ PROYECTO1/proyecto1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("proyecto1!");
+        }
+    }
+}
