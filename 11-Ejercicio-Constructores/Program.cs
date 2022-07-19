e35f4485e4ca96156428c121a2d497f6374ce1d0
shirlyperalta5 <shirlyperalta5@gmail.com>
2022-07-19 15:04:25 -0600

    Alumno.cs

---
+++ 11-Ejercicio-Constructores/Program.cs
@@ -0,0 +1,11 @@
+public class Alumno
+{
+    public  int Id { get; set;}\
+    public string PrimerNombre { get; set; }
+    public string SegundoNombre { get; set;}
+
+    public Alumno (){
+        Id = 1;
+    }
+    
+}
