e6ed23afa256562ef14bb6d10f6219167f6e00bb
shirlyperalta5 <shirlyperalta5@gmail.com>
2022-07-19 14:13:54 -0600

    AGREGANDO PROYECTO TAREA 2

---
+++ 11-Ejercicio-Constructores/Program.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace propidades
+{
+ class Program
+ {
+    static void Main (string[] args)
+    {
+        alumno a = new alumno();
+        a. Id = 1;
+        a.PrimerNombre ="Juan";
+        a.SegundoNombre ="Perez";
+
+        Alumno b = new alumno(); 
+        b. Id = 2;
+        b.PrimerNombre ="Maria";
+        b.SegundoNombre ="Martinez";
+
+        Alumno c = new alumno(); 
+        b. Id = 3;
+        b.PrimerNombre ="Pedro";
+        b.SegundoNombre ="Peralta";
+
+
+        Console.WriteLine(a.Id);
+        Console.WriteLine(b.Id);
+        Console.WriteLine(c.Id);
+
+    }
+    
+ }   
+
+}
