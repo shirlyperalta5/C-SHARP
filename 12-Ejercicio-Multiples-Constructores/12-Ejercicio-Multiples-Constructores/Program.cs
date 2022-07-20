a9f16e9cf34e256a1b472e803f07652ab3ebe0a0
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-20 11:55:59 -0600

    Add files via upload

---
+++ 12-Ejercicio-Multiples-Constructores/12-Ejercicio-Multiples-Constructores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12_Ejercicio_Multiples_Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
