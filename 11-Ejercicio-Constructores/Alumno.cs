6ef5d9355613ce80038b40a245cab119a124f626
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-20 11:54:03 -0600

    Add files via upload

---
+++ 11-Ejercicio-Constructores/Alumno.cs
@@ -0,0 +1,15 @@
+public class Alumno
+{
+    public  int Id { get; set;}\
+    public string PrimerNombre { get; set; }
+    public string SegundoNombre { get; set;}
+
+    public Alumno (){
+        Id = 1;
+    }
+    
+    public Alumno (int id){
+        Id = id;
+    }
+        Id = 1;
+}
