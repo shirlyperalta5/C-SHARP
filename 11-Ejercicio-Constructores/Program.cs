f80466fa8940e1889a0a8c0746c84986706ea090
shirlyperalta5 <shirlyperalta5@gmail.com>
2022-07-20 08:12:10 -0600

    agregando programa 12-Ejercicio-Multiples-Constructores

--- 11-Ejercicio-Constructores/Program.cs
+++ 11-Ejercicio-Constructores/Program.cs
@@ -1,11 +1,38 @@
-public class Alumno
-{
-    public  int Id { get; set;}\
-    public string PrimerNombre { get; set; }
-    public string SegundoNombre { get; set;}
+using System;
 
-    public Alumno (){
-        Id = 1;
+namespace propiedades
+{
+    class Program
+    {
+
+        static void Maid (string[] args)
+        {
+            Alumno  a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno  b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno  c = new Alumno(3);
+            c.Id = 3;
+            c.PrimerNombre = "Pedro";
+            c.SegundoNombre = "Peralta";
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            
+
+        }
+
+
+
     }
-    
+
+
+
 }
