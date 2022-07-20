e9bf52b3926e14fc5d8b9344922b820fce191df6
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-20 13:24:52 -0600

    Add files via upload

---
+++ 10-Ordenes/Vendedor.cs
@@ -0,0 +1,18 @@
+public class Vendedor: Persona
+
+{
+
+    public string CodigoVendedor { get; set; }
+
+
+    public Vendedor(int codigo, string nombre, string codigoVendedor)
+
+    {
+
+        Codigo = codigo;
+        Nombre = nombre;
+        CodigoVendedor = codigoVendedor;
+
+    }
+
+}
