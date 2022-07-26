95764ce5fbba691f032c0d9930928b3575038848
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-26 14:07:06 -0600

    Add files via upload

---
+++ 12-Ejercicio-Multiples-Constructores/Program.cs
@@ -0,0 +1,84 @@
+<<<<<<< HEAD
+using System;
+
+namespace propiedades
+{
+    class Program
+    {
+
+        static void Maid (string[] args)
+        {
+            Alumno  a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno  b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno  c = new Alumno(3);
+            c.Id = 3;
+            c.PrimerNombre = "Pedro";
+            c.SegundoNombre = "Peralta";
+
+            Alumno d = new Alumno("Juan", "Jimenez"); 
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d PrimerNombre + " " + d. SegundoNombre);
+            
+
+        }
+
+
+
+    }
+
+
+
+=======
+using System;
+
+namespace propiedades
+{
+    class Program
+    {
+
+        static void Maid (string[] args)
+        {
+            Alumno  a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno  b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno  c = new Alumno(3);
+            c.Id = 3;
+            c.PrimerNombre = "Pedro";
+            c.SegundoNombre = "Peralta";
+
+            Alumno d = new Alumno("Juan", "Jimenez"); 
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d PrimerNombre + " " + d. SegundoNombre);
+            
+
+        }
+
+
+
+    }
+
+
+
+>>>>>>> 7dab0a8954bbbba4e09d1a4b5c67048d6f7a5fc0
+}
