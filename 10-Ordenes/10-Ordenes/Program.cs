512ea6d35f9b053796549f4be9736ee598c17bbc
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-26 13:50:02 -0600

    Add files via upload

---
+++ 10-Ordenes/10-Ordenes/Program.cs
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+﻿using System;
+
+namespace _10_Ordenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+=======
+﻿using System;
+
+namespace _10_Ordenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+>>>>>>> 3834615de50cd7e0c2ee2f83a42d292d8014e8c1
---
+++ 10-Ordenes/ordenes-tarea/Cliente.cs
@@ -0,0 +1,16 @@
+public class Cliente: Persona
+
+{
+
+    public double Saldo { get; set; }
+    public Cliente(int codigo, string nombre, string telefono)
+
+    {
+
+        Codigo = codigo;
+        Nombre = nombre;
+        Telefono = telefono;
+
+    }
+
+}
