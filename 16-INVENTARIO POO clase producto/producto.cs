4e152db90ca5c46b5162ae8ec10cc912635f8610
shirlyperalta5 <98668594+shirlyperalta5@users.noreply.github.com>
2022-07-26 14:11:55 -0600

    Add files via upload

---
+++ 16-INVENTARIO POO clase producto/producto.cs
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+public class Producto
+{
+    public string Codigo { get; set; }
+    public string Descripcion { get, set,}
+    public int Existencia { get, set, }
+
+    public Producto (string codigo, string descripcion, int existencia)
+
+    {
+        Codigo = codigo;
+        Descripcion = descripcion;
+        Existencia = existencia;
+
+
+    }
+}
+
+=======
+public class Producto
+{
+    public string Codigo { get; set; }
+    public string Descripcion { get, set,}
+    public int Existencia { get, set, }
+
+    public Producto (string codigo, string descripcion, int existencia)
+
+    {
+        Codigo = codigo;
+        Descripcion = descripcion;
+        Existencia = existencia;
+
+
+    }
+}
+
+>>>>>>> 7dab0a8954bbbba4e09d1a4b5c67048d6f7a5fc0
